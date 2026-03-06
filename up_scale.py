2e48866b7d1958f60d1b4e076228400c4064e184
QT <qtests@outlook.com>
2026-03-07 00:10:26 +0100

    IMG CON v1.7

---
+++ up_scale.py
@@ -0,0 +1,14 @@
+
+# 1. https://www.youtube.com/@programmingdatascienceandother/videos - 2 videos
+
+# 2. https://github.com/mv-lab/swin2sr
+
+
+# https://huggingface.co/docs/transformers/main/en/model_doc/swin2sr#transformers.Swin2SRForImageSuperResolution.forward.example
+
+# https://aws.amazon.com/blogs/media/create-super-resolution-for-legacy-media-content-at-scale-with-generative-ai-and-aws/
+
+# https://medium.com/@mariallain/restoring-image-quality-with-ai-using-real-esrgan-and-swinir-20d54c483e39
+
+# https://www.youtube.com/watch?v=UAWPSPWHNlw
+
